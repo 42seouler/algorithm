6dc8ac4b37546ad0764cb589e638a868d513b488
onulhoi <onulhoi@onulhoiui-MacBookPro-4.local>
2021-07-27 22:25:04 +0900

    953. Verifying an Alien Dictionary

---
+++ leetcode/src/main/java/problem/MaxProfit.java
@@ -0,0 +1,28 @@
+package twosum;
+
+public class MaxProfit {
+//    public int maxProfit(int[] prices) {
+//        int result = 0;
+//        for (int i = 0; i < prices.length - 1; i++) {
+//            for (int j = i + 1; j < prices.length; j++) {
+//                int profit = prices[j] - prices[i];
+//                if (profit > result) {
+//                    result = profit;
+//                }
+//            }
+//        }
+//        return result;
+//    }
+    public int maxProfit(int[] prices) {
+        int minValue = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minValue) {
+                minValue = prices[i];
+            } else if (prices[i] - minValue > maxProfit) {
+                maxProfit = prices[i] - minValue;
+            }
+        }
+        return maxProfit;
+    }
+}
